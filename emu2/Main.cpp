6e0c8c1c811fcb94df7cea0b37931ea4a09f1183
Charlie Birks <charlie@daft.games>
2024-07-22 15:17:43 +0100

    emu2: convert CyID from string

--- emu2/Main.cpp
+++ emu2/Main.cpp
@@ -50,6 +50,81 @@ uint32_t crc32(uint8_t *data, int length)
 
 //
 
+static uint32_t cyIDFromString(const std::string &str)
+{
+    const uint8_t cyIDCharToIndex[]
+    {
+        0xFF, // 0
+        0xFF, // 1
+          24, // 2
+          25, // 3
+          26, // 4
+          27, // 5
+          28, // 6
+          29, // 7
+          30, // 8
+          31, // 9
+        0xFF, // :
+        0xFF, // ;
+        0xFF, // <
+        0xFF, // =
+        0xFF, // >
+        0xFF, // ?
+        0xFF, // @
+           0, // A
+           1, // B
+           2, // C
+           3, // D
+           4, // E
+           5, // F
+           6, // G
+           7, // H
+        0xFF, // I
+          23, // J
+           8, // K
+           9, // L
+          10, // M
+          11, // N
+        0xFF, // O
+          12, // P
+          22, // Q
+          13, // R
+          14, // S
+          15, // T
+          16, // U
+          17, // V
+          18, // W
+          19, // X
+          20, // Y
+          21, // Z
+    };
+
+    uint32_t cyID = 0;
+
+    // 5 bits per char
+    for(int i = 0; i < 6; i++)
+    {
+        int c = str[i] - '0';
+        if(c < 0 || c >= int(sizeof(cyIDCharToIndex)) || cyIDCharToIndex[c] == 0xFF)
+        {
+            std::cerr << "Invalid character \"" << str[i] << "\" in CyID!\n";
+            c = 'A' - '0';
+        }
+
+        cyID |= cyIDCharToIndex[c] << (26 - i * 5);
+    }
+
+    // only one bit for final char
+    if(str[6] == 'B')
+        cyID |= 1;
+    else if(str[6] != 'A')
+        std::cerr << "Invalid final character \"" << str[6] << "\" in CyID!\n";
+
+    // this seems to be a second bit for the final char, don't know if it has special meaning though
+    // cyID |= 1 << 31;
+
+    return cyID;
+}
 
 // dec 2019
 class PortA : public IODevice
@@ -299,10 +374,12 @@ int main(int argc, char *args[])
     // change id for fun
     if(xtreme)
     {
-        flash->write(0x7F818, 0x14);
-        flash->write(0x7F819, 0x08);
-        flash->write(0x7F81A, 0x20);
-        flash->write(0x7F81B, 0xA9);
+        uint32_t cyID = cyIDFromString("FAKECYB");
+
+        flash->write(0x7F818, cyID >> 24);
+        flash->write(0x7F819, cyID >> 16);
+        flash->write(0x7F81A, cyID >> 8);
+        flash->write(0x7F81B, cyID);
         uint32_t crc = ~crc32(flash->getData() + 0x7F800, 0x7FC);
         flash->write(0x7FFFC, crc >> 24);
         flash->write(0x7FFFD, crc >> 16);
